a14d21c4ea6c321c32045f925b51c2662c13a449
Nouchi <yuuta19950602@gmail.com>
2025-08-11 14:07:43 +0900

    feat: update User.ts
    Auto-committed by Claude Code at 14:07

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from 'mongodb';
-import { getDatabase } from '@/lib/mongodb';
+import { getDatabase } from '../../lib/mongodb';
 import bcrypt from 'bcryptjs';
 
 export interface UserDocument {
