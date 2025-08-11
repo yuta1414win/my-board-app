cf7ba1e5a43b452a5acc2153b0606b74e3e4ecd5
Nouchi <yuuta19950602@gmail.com>
2025-08-11 10:58:34 +0900

    feat: update jest.config.js
    Auto-committed by Claude Code at 10:58

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest/presets/default-esm',
   testPathIgnorePatterns: [
     '<rootDir>/.next/',
     '<rootDir>/node_modules/',
