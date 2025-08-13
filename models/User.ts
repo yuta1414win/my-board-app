68ee064660f035fcf9d96a202c305eb2f3ead3c9
Nouchi <yuuta19950602@gmail.com>
2025-08-14 08:10:33 +0900

    Revert "feat: update User.ts"
    This reverts commit bd56c1843ae4168c6a891e58f91c03f8df6deebc.

--- models/User.ts
+++ models/User.ts
@@ -2,6 +2,7 @@ import mongoose, { Document, Model, Schema } from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 export interface IUser extends Document {
+  _id: string;
   name: string;
   email: string;
   password: string;
