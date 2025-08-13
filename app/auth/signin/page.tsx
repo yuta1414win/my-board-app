350008b703a083cfb800c5685fefb735a2826c5a
Nouchi <yuuta19950602@gmail.com>
2025-08-14 08:10:33 +0900

    Revert "feat: update page.tsx"
    This reverts commit eb6cdb7bf756f802d3421db7611d56089e6bffd5.

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -3,9 +3,6 @@ import { redirect } from 'next/navigation';
 import { Box } from '@mui/material';
 import SignInForm from '../../../components/auth/sign-in-form';
 
-// 動的レンダリングを強制（cookiesを使用するため）
-export const dynamic = 'force-dynamic';
-
 export default async function SignInPage() {
   const session = await auth();
 
